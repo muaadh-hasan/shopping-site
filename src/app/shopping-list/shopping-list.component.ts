9c169163a97414d52671a0e2c1ce815288cd3c7a
Muaadh Hasan <36192122+muaadh-hasan@users.noreply.github.com>
2021-11-08 16:12:23 +0200

    added list shopping part.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients:Ingredient[] = [
+    new Ingredient("Apple",10),
+    new Ingredient("Orange",3),
+    new Ingredient("Banana",20),
+  ]
 
   constructor() { }
 
